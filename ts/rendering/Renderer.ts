577f096bb6b8a9c5cee02cf52b7cad1540b3feee
EricWehrly <eric@disruptiontheory.com>
2024-05-10 19:44:23 -0400

    initial debugging clicks

--- ts/rendering/Renderer.ts
+++ ts/rendering/Renderer.ts
@@ -6,9 +6,11 @@ import { Diagnostics } from './diagnostics';
 // TODO: Make static & Singleton
 export default class Renderer {
     renderer: THREE.WebGLRenderer;
-    terrain: Terrain;
     scene: THREE.Scene;
-    camera: THREE.Camera;
+    camera: THREE.Camera;    
+    raycaster: THREE.Raycaster;
+    
+    terrain: Terrain;
     diagnostics: Diagnostics;
 
     constructor(terrain: Terrain) {
@@ -17,6 +19,7 @@ export default class Renderer {
 
         this.scene = new THREE.Scene();
         this.renderer = new THREE.WebGLRenderer();
+        this.raycaster = new THREE.Raycaster();
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(this.renderer.domElement);
 
@@ -33,6 +36,8 @@ export default class Renderer {
 
         // TODO: if diagnostics is enabled
         this.diagnostics = new Diagnostics(this.renderer);
+        
+        window.addEventListener('mousedown', this.onMouseDown.bind(this), false);
 
         this.animate.bind(this)();
         console.log(this.renderer.info)
@@ -44,7 +49,42 @@ export default class Renderer {
         this.renderer.render(this.scene, this.camera);
         const elapsed = performance.now() - start;
         // TODO: if diagnostics is enabled
-        // this.diagnostics.update(elapsed);
         this.diagnostics.update(elapsed);
+    }
+
+    // we can probly move this to an "interaction" class
+    onMouseDown(event: MouseEvent) {
+
+        if(event.button !== 0) return; // Only handle left clicks (button 0)
+        
+        const lineMaterial = new THREE.LineBasicMaterial({ color: 0xff0000 });
+
+        // Normalize the mouse position from -1 to 1
+        const mouse = new THREE.Vector2();
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+        // Update the picking ray with the camera and mouse position
+        this.raycaster.setFromCamera(mouse, this.camera);
+
+        // Calculate objects intersecting the picking ray
+        const intersects = this.raycaster.intersectObjects(this.scene.children, true);
+
+        for (let i = 0; i < intersects.length; i++) {
+            // intersects[i].object is the intersected object
+            // We need to use "parent" in order to get the group that controls the object positioning
+            console.log(`Clicked on cube with position:`, intersects[i].object.parent.position);
+
+            if (intersects[i].object instanceof THREE.LineSegments) {
+                const lineSegments = intersects[i].object as THREE.LineSegments;
+                (lineSegments.material as THREE.LineBasicMaterial).color.set('pink');
+                
+                // Create a line from the camera to the point of intersection
+                const points = [this.camera.position, intersects[i].point];
+                const geometry = new THREE.BufferGeometry().setFromPoints(points);
+                const line = new THREE.Line(geometry, lineMaterial);
+                this.scene.add(line);
+            }
+        }
     }
 }
