cd3f0d54069e69cdf46115931e8e3c8039e7d046
EricWehrly <eric@disruptiontheory.com>
2024-05-10 17:58:41 -0400

    add dirt

--- ts/Tile.ts
+++ ts/Tile.ts
@@ -1,22 +1,25 @@
 import * as THREE from 'three';
 
 export enum TileMaterial {
+    Dirt,
     Grass,
     Water,
-    Stone,
+    Stone
 }
 
 export class Tile {
     material: TileMaterial;
     borderColor: THREE.Color;
 
-    constructor(material: TileMaterial = TileMaterial.Grass, borderColor?: THREE.Color) {
+    constructor(material: TileMaterial = TileMaterial.Dirt, borderColor?: THREE.Color) {
         this.material = material;
         this.borderColor = borderColor || new THREE.Color(0x000000); // Default to black if no color is provided
     }
 
     getMaterialColor(): THREE.Color {
         switch (this.material) {
+            case TileMaterial.Dirt:
+                return new THREE.Color(0x8B4513); // Saddle Brown
             case TileMaterial.Grass:
                 return new THREE.Color(0x228B22); // Forest Green
             case TileMaterial.Water:
