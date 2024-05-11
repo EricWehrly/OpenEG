6f9856907cde710f1e2c3e931bb3cd42e8a716f2
EricWehrly <eric@disruptiontheory.com>
2024-05-11 16:59:08 -0400

    categorize game objects and try to normalize casing

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './ts/main.ts',
+  entry: './ts/Main.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
